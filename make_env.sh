43fb10c56bb5dcbbc2558dd022e3a2452f68c38b
Paul Manis <pmanis@med.unc.edu>
2023-07-31 17:21:20 -0400

    some updates.

--- make_env.sh
+++ make_env.sh
@@ -12,7 +12,7 @@ else
     echo "No previous environment - ok to proceed"
 fi
 
-python3.10 -m venv $ENVNAME || exit 1
+python3.11 -m venv $ENVNAME || exit 1
 source $ENVNAME/bin/activate || exit 1
 python3 -m pip install --upgrade pip # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
