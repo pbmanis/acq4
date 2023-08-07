10ae0fece5b3235c3db97c3ea452c735c5ca399c
yuvrajjain2003 <yjain@unc.edu>
2023-08-07 13:10:53 -0400

    Fixed the cells being placed at (0, 0) Now cells are placed in the middle of the canvas

--- acq4/util/Canvas/items/CellCanvasItem.py
+++ acq4/util/Canvas/items/CellCanvasItem.py
@@ -24,19 +24,12 @@ class CellCanvasItem(CanvasItem):
         opts.setdefault('rotatable', False)
         CanvasItem.__init__(self, item, **opts)
         self.selectBox.addTranslateHandle([0.5,0.5])
-        # # print(dir(CanvasItem))
-        # # # print("\nself: \n", dir(self))
-        # # print("\nparent: ", self.parent)
-        # # print("\ndir parent : \n", dir(self.parent()))
-        # if 'position' in self.opts.keys():
-        #     position = self.opts['position']
-        # else:
-        #     position = (0,0,0)
-        #     vr = self.parentItem().graphicsItem().viewRect()
-        #     if vr is not None:
-        #         position = [vr.center().x(), vr.center().y(), 0]
 
-        self.graphicsItem().setPos(0.0, 0.0)
+        center_pos = opts['viewRect'].center()
+
+        print(center_pos)
+
+        self.graphicsItem().setPos(center_pos.x(), center_pos.y())
     
     @classmethod
     def checkFile(cls, fh):
