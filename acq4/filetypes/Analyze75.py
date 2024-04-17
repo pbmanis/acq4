c4807e6c33cb4c252e9faf319bd89656ba8c2a55
ManisLab_Rig4 <pmanis@med.unc.edu>
2024-04-17 17:52:37 -0400

    Fix deprecated numpy aliases

--- acq4/filetypes/Analyze75.py
+++ acq4/filetypes/Analyze75.py
@@ -34,15 +34,15 @@ class Analyze75(FileType):
 
 
 ## Function for reading NiFTI-1 and ANALYZE 7.5 image formats  (.nii and .hdr/.img files)
-
+# correct for deprecations in numpy >= 1.20 (int, complex)
 dataTypes = {
     0: None,
     1: 'BINARY',
     2: np.ubyte,
     4: np.short,
-    8: np.int,
+    8: int,
     16: np.float32,
-    32: np.complex,   ## two floats: (re, im)
+    32: complex,   ## two floats: (re, im)
     64: np.float64,
     128: [('R', np.ubyte), ('G', np.ubyte), ('B', np.ubyte)],
     256: 'ALL'
